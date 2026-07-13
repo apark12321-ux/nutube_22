6f5e0d630759ef0f8e97b792c41c9538ac316383
apark12321-ux <apark12321@gmail.com>
2026-07-13 13:19:20 +0900

    refactor: rename brand to 나우크리에이터랩
    Update brand identity from 크리에이터 가이드랩 to 나우크리에이터랩 across site metadata and application constants.

--- src/brand.ts
+++ src/brand.ts
@@ -1,5 +1,5 @@
-export const SITE_DISPLAY_NAME = '크리에이터랩';
-export const SITE_ENGLISH_NAME = 'Creator Lab';
+export const SITE_DISPLAY_NAME = '나우크리에이터랩';
+export const SITE_ENGLISH_NAME = 'Now Creator Lab';
 
 const replaceText = (value: string) =>
   value
